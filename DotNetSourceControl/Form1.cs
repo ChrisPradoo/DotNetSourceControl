0f41d57e78c093c1a8c657a1fd3d8322eb28ed0e
Christian Castañeda Prado <ChristianPrado5@outlook.com>
2026-04-15 13:39:31 -0700

    Designed the main form

--- DotNetSourceControl/Form1.cs
+++ DotNetSourceControl/Form1.cs
@@ -6,5 +6,10 @@ namespace DotNetSourceControl
         {
             InitializeComponent();
         }
+
+        private void btnGreeting_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
